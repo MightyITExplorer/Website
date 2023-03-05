b23273925ee6344a9ad2fe5664250ea389045d69
Awesomeasma <12006052@svuceedu.onmicrosoft.com>
2023-03-05 22:40:15 +0530

    updated the report-bug-page

--- report-bug.js
+++ report-bug.js
@@ -5,7 +5,7 @@ const errorMessage = document.querySelector('#error-message');
 form.addEventListener('submit', async event => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const response = await fetch('/submit-bug-report', {
+    const response = await fetch('http://mockbin.com/bin/7eb970ae-1814-4394-88d4-a51cc1568443', {
         method: 'POST',
         body: formData
     });
